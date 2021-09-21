da5bfea9e108c080d642ba2af7d37372dcceadce
jhocardoso <jhonatancardoso17@hotmail.com>
2021-09-21 23:22:22 +0000

    (21/09) Exercício 03

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -3,6 +3,8 @@ import Teste1 from './exercicios/Teste1';
 import Teste2 from './exercicios/Teste2';
 import Exercicio01 from './exercicios/01'
 import Exercicio02 from './exercicios/02'
+import Exercicio03 from './exercicios/03';
+import Exercicio04 from './exercicios/04';
 
 import './App.css';
 
@@ -28,6 +30,12 @@ function App() {
           <li>
             <Link to="ex02">Exercício 02</Link>
           </li>
+          <li>
+            <Link to="ex03">Exercício 03</Link>
+          </li>
+          <li>
+            <Link to="ex04">Exercício 04</Link>
+          </li>
         </ul>
 
         <div style={{ border: '1px solid blue', padding: '20px'}}>
@@ -43,6 +51,12 @@ function App() {
             </Route>
             <Route path="/ex02">
               <Exercicio02/>
+            </Route>
+            <Route path="/ex03">
+              <Exercicio03/>
+            </Route>
+            <Route path="/ex04">
+              <Exercicio04/>
             </Route>
           </Switch>
         </div>
